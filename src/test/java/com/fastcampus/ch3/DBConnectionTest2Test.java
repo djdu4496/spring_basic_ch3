5c910d1ffb38c733cab4da0425bc7277a5ad2d35
djdu4 <djdu4496@gmail.com>
2022-07-26 23:29:28 +0900

    Ch. 03 Spring Di와 AOP 15.

--- src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
+++ src/test/java/com/fastcampus/ch3/DBConnectionTest2Test.java
@@ -72,7 +72,7 @@ public class DBConnectionTest2Test {
    public int updateUser(User user) throws Exception {
         Connection conn = ds.getConnection();
 
-        String sql = "update user_info set pwd=?, name=?, email=?, birth=?, sns=? where id= ?";
+        String sql = "update user_info set pwd=?, name=?, email=?, birth=?, sns=?, reg_date=? where id= ?";
 
         PreparedStatement pstmt = conn.prepareStatement(sql);
         pstmt.setString(1, user.getPwd());
@@ -80,7 +80,8 @@ public class DBConnectionTest2Test {
         pstmt.setString(3, user.getEmail());
         pstmt.setDate(4, new java.sql.Date(user.getBirth().getTime()));
         pstmt.setString(5, user.getSns());
-        pstmt.setString(6, user.getId());
+        pstmt.setTimestamp(6, new java.sql.Timestamp(user.getReg_date().getTime()));
+        pstmt.setString(7, user.getId());
 
         int rowCnt = pstmt.executeUpdate();
         return rowCnt;
